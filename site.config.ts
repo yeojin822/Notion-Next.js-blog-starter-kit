abe2e715aa9adab184195bc80380d2b3b6e938b1
yeojin822 <xlzt2002@gmail.com>
2023-07-11 15:05:26 +0900

    fix : 주소변경

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'yeojin',
 
   // open graph metadata (optional)
-  description: 'yeojin blog - developer blog',
+  description: 'Yeojin blog',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
