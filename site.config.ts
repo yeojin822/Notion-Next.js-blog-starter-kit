5442fb399a97af3a98f238e3d7104c0854bcb6cc
yeojin822 <xlzt2002@gmail.com>
2023-07-14 18:42:28 +0900

    fix : wip

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: "yeojinlee",
 
   // basic site info (required)
   name: 'Yeojin blog',
